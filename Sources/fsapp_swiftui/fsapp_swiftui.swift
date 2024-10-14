ff37ee67aed2e3181b26ddc30620fd9a6039f9dc
Josh Gordon <josh.gordon@fox.com>
2024-10-14 12:44:04 -0400

    swift package setup

---
+++ Sources/fsapp_swiftui/fsapp_swiftui.swift
@@ -0,0 +1,17 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!!!!!")
+            .padding(40)
+            .background(Color.red)
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
